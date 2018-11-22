e8181142194be4d07909f20ea3fd0e5a35d1ad1e
Karl <karl@unknown.home>
2018-11-22 20:55:53 +0000

    Add (commented-out) options for execution stats

--- src/data-access/books.js
+++ src/data-access/books.js
@@ -3,7 +3,11 @@ const Book = require('../models/book')
 const m = {
   // List all books with their authors.
   all: async () => {
-    const data = await Book.find({}).populate(['author', 'genre']).sort({ title: 1 }).lean()
+    const data = await Book.find({})
+      .populate(['author', 'genre'])
+      .sort({ title: 1 })
+      .lean()
+    //   .explain('executionStats')
     return data
   }
 }
--- src/data-access/genres.js
+++ src/data-access/genres.js
@@ -3,7 +3,11 @@ const Genre = require('../models/genre')
 const m = {
   // List all genres with their books.
   all: async () => {
-    const data = await Genre.find({}).populate('books').sort({ title: 1 }).lean()
+    const data = await Genre.find({})
+      .populate('books')
+      .sort({ title: 1 })
+      .lean()
+    //   .explain('executionStats')
     return data
   }
 }
