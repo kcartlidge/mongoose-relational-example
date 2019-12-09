77c32a97c34efd8f2b51905afed3211986cc9e78
K Cartlidge <code@kcartlidge.com>
2019-12-09 12:47:56 +0000

    Add skeleton api server

--- README.md
+++ README.md
@@ -9,6 +9,16 @@ Mongo (and Mongoose) backed API example.
     - editorconfig.editorconfig
     - dbaeumer.vscode-eslint
 
+## Running the example
+
+Written with Node v12.13.1 (LTS).
+
+``` sh
+npm install     # pull dependencies
+npm start       # run the example
+npm run lint    # tidy up the 'src' folder code
+npm run watch   # run and restart on changes
+
 ## Notes
 
 Dependencies for `es-lint` added via:
--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log('Hello, cruel world.')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const port = 3000
+
+const app = express()
+app
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .get('/', (req, res) => { res.send('Okay') })
+
+app.listen(port, () => {
+  console.log(' - http://localhost:' + port)
+  console.log('LISTENING ...')
+})
